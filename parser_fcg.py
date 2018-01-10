e70685ca90958c49487a133faad2699ec8e92604
fcg <thss15_fangcg@163.com>
2018-01-11 03:11:08 +0800

    finishi for_instr and selection_instr

---
+++ parser_fcg.py
@@ -0,0 +1,14 @@
+def p_for_instr(p):
+    ''' for_instr: FOR '(' expression_opt ';' expression_opt ';' expression_opt ')' '{' instruction_list '}'
+                 | FOR '(' expression_opt ';' expression_opt ';' expression_opt ')' instruction '''
+
+def p_selection_instr(p):
+    ''' selection_instr : IF '(' expression ')' '{' instruction_list '}' ELSE '{' instruction_list '}' 
+                        | IF '(' expression ')' '{' instruction_list '}' ELSE instruction 
+                        | IF '(' expression ')' '{' instruction_list '}'   
+                        | IF '(' expression ')' instruction ELSE '{' instruction_list '}' 
+                        | IF '(' expression ')' instruction ELSE instruction
+                        | IF '(' expression ')' instruction'''
+
+def p_expr_opt(p):
+    ''' expression_opt : expressinon | empty '''
